b4277c6010f640f75d07a215d542376179bc7bbf
SnowCait <SnowCait@users.noreply.github.com>
2022-08-13 19:42:54 +0900

    openapi.json URLs

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,10 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    urls: [
+      {url: `${location.href}openapi.v2.json`, name: 'Twitter API v2'},
+      {url: `${location.href}openapi.labs.json`, name: 'Twitter API Labs'},
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
